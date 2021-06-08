7ef5c63a1e13349470df5ed36b1a03e5e7888818
William Mann <WMANN>
2021-06-08 16:26:40 +0200

    :mag:

--- include/scan.h
+++ include/scan.h
@@ -7,4 +7,18 @@
 
 void my_find(char *needle, char *haystack);
 
+/*
+ *  Function that recursivly reads and get the informations of the repository
+ *  char* path - string which represents the path to verify
+ *  return - s_directory containing the informations of the directory
+ */
+s_directory *process_dir(char *path);
+
+/*
+ *  Function that recursivly reads and get the informations of the file
+ *  char* path - string which represents the path to verify
+ *  return - s_file containing the informations of the file
+ */
+s_file *process_file(char *path);
+
 #endif
---
+++ include/tree.h
@@ -0,0 +1,34 @@
+#ifndef TREE
+#define TREE
+
+#include "type.h"
+
+/*
+ *  Function that appends a subdirectory to a directory
+ *  s_directory *child - directory child to append
+ *  s_directory *parent - directory in which we append
+ *  return - int, 0 if there is an error, else 1
+ */
+int append_subdir(s_directory *child, s_directory *parent);
+
+/*
+ *  Function that appends a file to a directory
+ *  s_directory *child - directory child to append
+ *  s_directory *parent - directory in which we append
+ *  return - int, 0 if there is an error, else 1
+ */
+int append_file(s_file *child, s_directory *parent);
+
+/*
+ *  Function that clears the memory of the files
+ *  s_directory *parent - pointer to clear
+ */
+void clear_files(s_directory *parent);
+
+/*
+ *  Function that clears the memory of the subdirectories
+ *  s_directory *parent - pointer to clear
+ */
+void clear_subdirs(s_directory *parent);
+
+#endif
