1818dcdf3667b3cfc34774e12e0a7d4953271584
0utraged <erwann@leguilly.free.fr>
2021-06-09 18:21:02 +0000

    MD5 sum .c and .h added

---
+++ include/md5sum.h
@@ -0,0 +1,18 @@
+#ifndef MD5SUM_HEADER
+#define MD5SUM_HEADER
+#include <stdbool.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <inttypes.h>
+#include <openssl/md5.h>
+
+//Generates the MD5 sum of a file and store it in a buffer
+int compute_md5(char *path, unsigned char buffer[]);
+
+//converts an MD5 string into hexa, placing it into an array
+void md5_to_hexa(unsigned char md5_array[], char* md5_old);
+
+#endif
