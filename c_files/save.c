ffdd37fe538d0e6c8c7eddc07aaf4d396d4b1277
Jules <jules.ramos@utbm.fr>
2021-06-09 12:31:55 +0200

    save.c done

--- c_files/save.c
+++ c_files/save.c
@@ -17,7 +17,7 @@ int save_to_file(s_directory *root, char *path_to_target){
     strftime(str_time, 20, "%Y-%m-%d %H:%M:%S", localtime(root.mod_time));
 
     //write in the save file all the expected data
-	fprintf(f,"Directory:\n%d\t%s\t%s/\n",root.file_type,str_time,root.name);
+	fprintf(f,"Directory:\n0\t%s\t%s/\n",str_time,root.name);
 
 	//display all files, not checking if the files pointer is null because it is done by the function already
 	if(write_files(root.files,path_to_target) == 0){
@@ -30,10 +30,11 @@ int save_to_file(s_directory *root, char *path_to_target){
 	}
 
 	printf("\n");
+	fclose(f);
+
 	//time to get recursive, we will first display all the data of subdirectories if there are some:
 	if (root.subdirs != NULL){
 		if (save_to_file(root.subdirs,path_to_target) == 0){
-			fclose(f);
 			return 0;
 		}
 	}
@@ -41,19 +42,78 @@ int save_to_file(s_directory *root, char *path_to_target){
 	//and now for the next directory of this level:
 	if(root.next_dir != NULL){
 		if (save_to_file(root.next_dir,path_to_target) == 0){
-			fclose(f);
 			return 0;
 		}
 	}
 
-	fclose(f);
-	return 1;
+	return -1;
 }
 
-int write_files(s_directory *file, char *path_to_target){
-	return 0;
+int write_files(s_file *file, char *path_to_target){
+	if(file == NULL){
+		return 0;
+	}
+
+	FILE *f = fopen(path_to_target, "w"); // Open in text mode, for writing
+    if (!f){ //error should not happen at this point but still checking juuuuuuust in case you know
+    	return 0;
+    }
+
+    //get a proper displayable date for the current directory
+    char str_time[20];
+    strftime(str_time, 20, "%Y-%m-%d %H:%M:%S", localtime(file.mod_time));
+
+    //write in the save file all the expected data
+    fprintf(f,"%d\t%s\t",file.file_type,str_time);
+
+    if(file.file_type == 1){
+    	fprintf("%d\t",file.file_size);
+    } 
+
+    if(md5_sum_computing){
+    	fprintf("%s\t",file.md5sum);
+    }
+
+    fprintf("%s\n",file.name);
+
+	fclose (f);
+
+	//recursively write all files in the linked list
+	if(file.next_file != NULL){
+		if(write_files(file.next_file,path_to_target) == 0){
+			return 0;
+		}
+	}
+
+	return -1;
 }
 
+//similar code to save_to_file but without the files and double recursivity
 int write_directories(s_directory *dir, char *path_to_target){
-	return 0;
+	if (dir == NULL){
+		return 0;
+	}
+
+	FILE *f = fopen(path_to_target, "w"); // Open in text mode, for writing
+    if (!f){ //error should not happen at this point but still checking juuuuuuust in case you know
+    	return 0;
+    }
+
+    //get a proper displayable date for the current directory
+    char str_time[20];
+    strftime(str_time, 20, "%Y-%m-%d %H:%M:%S", localtime(dir.mod_time));
+
+    //write in the save file all the expected data
+	fprintf(f,"0\t%s\t%s/\n",str_time,dir.name);
+
+	fclose(f);
+
+	//recursively write all subdirectories
+	if(dir.next_dir != NULL){
+		if (write_directories(dir.next_dir,path_to_target) == 0){
+			return 0;
+		}
+	}
+
+	return -1;
 }
