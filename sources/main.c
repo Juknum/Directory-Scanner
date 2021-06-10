89b0e6dd041215396115e78c28dd6a6ced071a67
antoine <antoine@reziliable.home>
2021-06-10 12:00:48 +0200

    fonction main update

--- sources/main.c
+++ sources/main.c
@@ -3,7 +3,77 @@
  * @param argc Nombre d'arguments
  * @param argv Tableau des arguemnts
  */
-int main(int argc, char const *argv[]) {
-  /* code */
-  return 0;
+#include <unistd.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+	char *file, *directory;
+	short is_file = 0, is_dir = 0, opt = 0, check_md5 = 0;
+	
+	while((opt = getopt(argc, argv, "o:si:")) != -1) 
+	{
+		switch (opt) 
+		{
+			case 'o': //obtenir le nom du fichier de sauvegarde
+				file = malloc(sizeof(char) * strlen(optarg));
+				strcpy(file,optarg);
+				is_file = 1;
+				printf("-> saving in file %s\n", file);
+				break;
+				
+			case 's'://activation du calcul des sommes MD5
+				check_md5 = 1;//Vaut 1 si la somme doit etre calculé, 0 sinon
+				printf("-> activating MD5 calculus \n");
+				break;
+
+			case 'i'://obtenir le répertoire à analyser
+				;
+				DIR *test_dir = opendir(optarg);//Vérifie l'existence du répertoire
+				if (!test_dir) 
+				{
+					printf("%s is not a valid path\n", optarg);
+					return 1;
+				}
+				closedir(test_dir);
+				directory = malloc(sizeof(char) * strlen(optarg));
+				strcpy(directory,optarg);
+				is_dir = 1;
+				printf("-> analysing directory: %s\n", directory);
+				break;
+
+			default:
+				break;
+		}
+	}
+	if (is_file == 0)//Si aucun fichier n'a été précisé, création du fichier de sauvegarde
+	{
+		FILE *save_file = NULL;
+		save_file = fopen("save_file.txt", "a");
+		if (save_file != NULL)
+		{
+			file = malloc(sizeof(char) * strlen("save_file.txt"));
+			strcpy(file,"save_file.txt");
+			printf("-> saving in file %s\n", file);
+			fclose(save_file);
+		}else
+		{
+			printf("can't create save_file.txt\n");
+			return 1;
+		}
+	}
+	if (is_dir == 0)//Si aucun répertoire n'a été précisé, analyse du répertoire courant "."
+	{
+		directory = malloc(sizeof(char) * strlen("."));
+		strcpy(directory,".");
+		printf("-> analysing directory: %s\n",directory);
+	}
+	//programme(file,directory,check_md5)
+	return 0;
 }
+
+// /!\ouvrir le fichier de sauvegarde en mode "a"
