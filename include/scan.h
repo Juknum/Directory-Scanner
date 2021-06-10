99ebddb62e11f0255a1fa4c23184f97d8461aca0
Clamorae <trombiniquentin@gmail.com>
2021-06-10 13:57:00 +0200

    scan maybe good

--- include/scan.h
+++ include/scan.h
@@ -8,19 +8,23 @@
 #include <dirent.h>
 #include <string.h>
 #include <sys/stat.h>
+
+/*
+ *  Function that recursivly reads directory and call s_directory *process_dir(char *path)
+ *  to get information about directory or,s_file *process_file(char *path); to get 
+ *  information about files.
+ */
 void scan(char *dir_name);
 
 /*
- *  Function that recursivly reads and get the informations of the repository
- *  char* path - string which represents the path to verify
- *  return - s_directory containing the informations of the directory
+ *  Function that will get a path to a directory and return a s_directoy variable, filled 
+ *  with all the information about this directory.
  */
 s_directory *process_dir(char *path);
 
 /*
- *  Function that recursivly reads and get the informations of the file
- *  char* path - string which represents the path to verify
- *  return - s_file containing the informations of the file
+ *  Function that get a path to a file and return a s_file, filled with all the 
+ *  information about this file.
  */
 s_file *process_file(char *path);
 
