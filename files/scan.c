c83d16fd9b16005e702c6402a5ea441fa908a4e9
William Mann <WMANN>
2021-06-12 21:12:39 +0200

    Bug dernier fichier

--- files/scan.c
+++ files/scan.c
@@ -2,21 +2,29 @@
 
 void scan(char *dir_name,s_directory *parent) {
     DIR *directory = opendir(dir_name);
+    s_directory *currentDir = (s_directory*)malloc(sizeof(s_directory));
+    s_file* currentFile = (s_file*)malloc(sizeof(s_file));
+    char new_path[MAX_HANDLED_PATH_LENGTH];
+    printf("Scanning %s\n",dir_name);
     if (directory) {
         struct dirent *current_entry;
         struct stat current_stat;
         while ((current_entry = readdir(directory)) != NULL) {
             if (strcmp(current_entry->d_name, ".") == 0 || strcmp(current_entry->d_name, "..") == 0)
                 continue;
-            char new_path[MAX_HANDLED_PATH_LENGTH];
+            strcpy(new_path,"");
             if (snprintf(new_path, MAX_HANDLED_PATH_LENGTH, "%s/%s", dir_name, current_entry->d_name) < MAX_HANDLED_PATH_LENGTH) {
+                printf("New path : %s\n",new_path);
                 if (lstat(new_path, &current_stat) != -1) {
                     if (S_ISDIR(current_stat.st_mode)) {
-                        s_directory *current=process_dir(new_path);
-                        append_subdir(current,parent);
-                        scan(new_path,current);
+                        currentDir=process_dir(new_path);
+                        append_subdir(currentDir,parent);
+                        scan(new_path,currentDir);
+                        currentDir = NULL;
                     }else{
-                        append_file(process_file(new_path),parent);
+                        currentFile = process_file(new_path);
+                        append_file(currentFile,parent);
+                        currentFile = NULL;
                     }
                 }
             }
@@ -26,6 +34,8 @@ void scan(char *dir_name,s_directory *parent) {
         printf("Error scan/scan ! Directory not found.\n");
         free(directory);
     }
+    free(currentDir);
+    free(currentFile);
 }
 
 s_directory *process_dir(char *path){
@@ -52,6 +62,7 @@ s_file *process_file(char *path){
         strcpy(new_file->name,path);
         new_file->mod_time=buffer.st_mtime;
         new_file->file_size=buffer.st_size;
+        new_file->next_file = NULL;
     }
     
 
