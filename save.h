00abbbadb95a51592ce2a95637fc896c0b64d1e4
youri aubry <youri.aubry@utbm.fr>
2021-06-13 17:01:43 +0200

    No warning update

--- save.h
+++ save.h
@@ -1,16 +1,10 @@
+#include <stdbool.h>
+#include <time.h>
+#include <string.h>
+#include <linux/limits.h>
+
 #ifndef _SAVE_H
 #define _SAVE_H
-
-#include "scan.h"
-#include <stdbool.h>
-
-bool do_arborescence = false;
-bool do_path = false;
-bool do_md5sum = false;
-
-int save_to_file(s_directory *root, char *path_to_target);
-void write_file_line(FILE *target_file,s_file *my_file,char *path,int depth);
-void write_dir_line(FILE *target_file,s_directory* my_dir,char *path,int depth);
 
 int save_to_file(s_directory *root, char *path_to_target, char *path_to_dir);
 void print_dir(FILE *target_file, s_directory *my_dir, char *path, int depth);
