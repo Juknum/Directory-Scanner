9946a92066cba2c3ebeb5bcc7900c7a68c8cfea7
youri aubry <youri.aubry@utbm.fr>
2021-06-14 23:52:46 +0200

    Comment update

--- md5sum.c
+++ md5sum.c
@@ -1,43 +1,53 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <openssl/md5.h>
 #include "md5sum.h"
 
 
-int md5sum(char* path, unsigned char buffer[]) {
-	int n;
-	MD5_CTX c;
-	long size;
-	char *out = (char*)malloc(33);
-	FILE* f  = NULL;
-	f= fopen(path,"rb");
-	
-	fseek(f,0,SEEK_END);
-	size = ftell(f);
-	
-	MD5_Init(&c);
+//Compute the md5sum of a file manually using openssl or bsd library
+/**int compute_md5sum(char *path, unsigned char buffer[]){
+    // Reads the file in binary mode
+    FILE *file=fopen(path,"rb");
+    if(!file){
+        printf("File %s couldn't be opened\n\n", path);
+        fprintf(stderr, "Error while opening %s", path);
+        return -1;
+    }
+	uint8_t temp_buffer[512];
+	ssize_t bytes;
+    MD5_CTX ctx;
+    // Initialize the structure used for md5sum computing
+	CC_MD5_Init(&ctx);
+    // read the file 512 bytes at a time
+	while((bytes=fread(temp_buffer, 1, 512, file)) > 0){
+		// Add the 512 bytes part of the file to the total md5sum 
+        CC_MD5_Update(&ctx, temp_buffer, bytes);
+    }
+    // End the md5sum computation and stock the result in the buffer
+	CC_MD5_Final(buffer, &ctx);
+	fclose(file);
+    return 0;
+}**/
 
-	while (size > 0)
-	{
-		if (size > 512 )
-		{
-			MD5_Update(&c,buffer,512);
-		}else{
-			MD5_Update(&c,buffer,size);
-		}
-		size -=512;
-		
-	}
-	MD5_Final(buffer, &c);
-	
-	for (n = 0; n < 16; ++n) {
-		snprintf(&(out[n*2]), 16*2, "%02x", (unsigned int)buffer[n]);
-	}
-	printf("%s\n", out);
-	
-    fclose(f);
+//compute the md5sum of a file using a bash command
+int compute_md5sum(char *path, char buffer[]){
+    char *command_line= malloc(sizeof(char)*(strlen(path)+8));
+    char *temp_buffer= malloc(sizeof(char)*(strlen(path)+34));
+    // Write the command line to send to the terminal
+    sprintf(command_line, "md5sum %s", path);
+    // Sends the command to the terminal
+    FILE *system_stream = popen(command_line, "r");   
+    if(!system_stream){
+        free(command_line);
+        free(temp_buffer);
+        return -1;
+    }
+    // Reads the content of the system stream while there is content
+    while(fgets(buffer,PATH_MAX+33, system_stream) != 0){ ; }
+    strcpy(buffer, temp_buffer);
+    free(temp_buffer);
+    free(command_line);
+    if(pclose(system_stream) == -1){
+        printf("Error while closing file stream\n");
+        return -1;
+    }
     return 0;
 }
-
-
+    
