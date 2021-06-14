f89b1890ca1bc0906739c59e5aa15852449ee829
youri aubry <youri.aubry@utbm.fr>
2021-06-14 23:53:43 +0200

    comment update

--- md5sum.h
+++ md5sum.h
@@ -1,11 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <bsd/md5.h>
+#include <linux/limits.h>
+#include <string.h>
+#include <sys/types.h>
+
 #ifndef _md5sum_H
 #define _md5sum_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <openssl/md5.h>
-
-int md5sum(char* path, unsigned char buffer[]);
+/**
+*@param path : path of the file to compute
+*@param buffer : string to store the md5sum 
+*@return -1 if it goes wrong, 0 otherwise
+**/
+int compute_md5sum(char* path, char buffer[]);
 
 #endif
