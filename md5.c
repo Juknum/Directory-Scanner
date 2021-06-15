5a3c2158e6af7b0aca6d96dc0e90b689764bd5b1
samuel bernard <samuel.bernard@utbm.fr>
2021-06-15 23:11:51 +0200

    Update md5.c

--- md5.c
+++ md5.c
@@ -6,41 +6,36 @@
 #include "openssl/md5.h"
 #include "md5.h"
 #include <linux/limits.h>
-int main(int argc , char **argv){
-    unsigned char buffer;
-    compute_md5("/home/ebessah/Bureau/save.scan",buffer);
-}
+
+
+
 int compute_md5(char *path, unsigned char buffer[]){
 
     // calcul de la somme md5 d'un fichier 
-    // le buffer represente le digest
-    //stocke MD5sum
+    // le buffer represente le digest et stocke la somme MD5
+
     char buf[512];
 
-        //ouverture fichier  en lecture seule => O_RDONLY
-        int file = open(path,O_RDONLY); 
+    //ouverture du fichier en paramètre en lecture seule 
+    int file = open(path,O_RDONLY); 
 
     MD5_CTX c;
-        ssize_t bytes;
     MD5_Init(&c);
+    ssize_t bytes;
 
     //lecture du fichier
-        bytes=read(file, buf, 512);   //lit 512 octets dans le fichier désigné par file et les met dans buf.  bytes prend la valeur du nombre d octet lus
+    bytes=read(file, buf, 512);   //lit 512 octets dans le fichier désigné par file et les met dans buf.  bytes prend la valeur du nombre d octet lus
 
-        while(bytes > 0) //lecture complète du fichier
+    while(bytes > 0) //lecture complète du fichier
         {
-                MD5_Update(&c, buf, bytes);
-                bytes=read(STDIN_FILENO, buf, 512);   //lit 512 octets dans le fichier désigné par file et les met dans buf.  bytes prend la valeur du nombre d octet lus
+                MD5_Update(&c, buf, bytes);   // mise à jour tant que tous les octets n ont pas été lus
+                bytes=read(file, buf, 512);   //lit 512 octets dans le fichier désigné par file et les met dans buf.  bytes prend la valeur du nombre d octet lus
         }
 
-        MD5_Final(buffer, &c);
+    //copie de la somme MD5 dans buffer
+    MD5_Final(buffer, &c);
 
     //fermeture du fichier
-        close(file);
+    close(file);
 
-        for(int n=0; n<MD5_DIGEST_LENGTH; n++)
-                printf("%02x", buffer[n]); //affichage de MD5sum
- 
-        return 0;
 }
-
